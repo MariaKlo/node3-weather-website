c2f8bb713753b73aaeed0ad00a9c7815aae5df9b
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2020-10-31 20:59:49 +0300

    Add extra info for forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,12 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `The weather is ${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`)
+            callback(undefined, 
+                `The weather is ${body.current.weather_descriptions[0]}. 
+                It is currently ${body.current.temperature} degrees out. 
+                It feels like ${body.current.feelslike} degrees out. 
+                The humidity is ${body.current.humidity}%. 
+                The wind speed is ${body.current.wind_speed} kmph.`)
         }
     })
 }
